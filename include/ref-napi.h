85cf9adc9d073e102ff7ac919d3ae0957da1f70d
Ravi Shrestha <ravi.shrestha@sussol.net>
2021-05-22 19:04:36 +0545

    Indentation update

--- include/ref-napi.h
+++ include/ref-napi.h
@@ -9,14 +9,12 @@
 // The definitions in this file are intended to be used by node-ffi-napi.
 
 namespace RefNapi {
-
-class Instance {
- public:
-  virtual napi_value WrapPointer(char* ptr, size_t length) = 0;
-  virtual char* GetBufferData(napi_value val) = 0;
-  virtual void RegisterArrayBuffer(napi_value val) = 0;
-};
-
+  class Instance {
+    public:
+    virtual napi_value WrapPointer(char* ptr, size_t length) = 0;
+    virtual char* GetBufferData(napi_value val) = 0;
+    virtual void RegisterArrayBuffer(napi_value val) = 0;
+  };
 }
 
 #endif
